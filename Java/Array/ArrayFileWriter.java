d536d517ccbaa3bb2dc3b1e350e7985fe05ea114
b-hyoung <youqlrqod@gmail.com>
2025-03-27 10:02:56 +0900

    initial Data

---
+++ Java/Array/ArrayFileWriter.java
@@ -0,0 +1,28 @@
+import java.io.FileWriter; // 파일에 데이터 쓰기 기능을 제공하는 클래스
+import java.io.IOException;  //파일 입출력 예외처리 클래스
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+
+public class ArrayFileWriter { //프로그램의 시작점 항상 파일이름과 동일해야함
+    public static void main(String[] args) throws Exception{
+        int[] numbers = {10,20,40,60};
+        String timeStemp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String fileName = "array_"+timeStemp+".txt";
+        try{
+            /*
+            "array_data.txt"라는 파일을 새로 만들거나 열어서 데이터를 쓸 준비를 해.
+    •	    파일이 현재 디렉토리에 없으면 새로 생성돼.
+            경로 포함 파일 이름도 가능 (예: "./Java/Array/data.txt")
+            */
+            FileWriter writer = new FileWriter(fileName);
+            for (int num : numbers ){
+                writer.write(num+"\n");
+            }
+            writer.close();
+            System.out.println("배열 저장 완료");
+    }catch(IOException e){
+        e.printStackTrace();
+    }
+}
+}
---
+++ Java/Array/ReadMe.md
@@ -0,0 +1,3 @@
+ArrayFileWriter.java   ✅ 배열 → 파일로 저장
+ArrayFileReader.java   ✅ 파일 → 배열로 불러오기
+ArrayIOTest.java       ✅ 입출력 + 테스트용
